4b5586fa5778ffd6fe6b4352d90e975e4e4875e9
SometimesSquishy <91513394+SometimesSquishy@users.noreply.github.com>
2022-03-28 16:22:04 -0400

    Update install.sh

--- i3installconfig/install.sh
+++ i3installconfig/install.sh
@@ -1,4 +1,7 @@
 ## Run as Root
+#!/bin/sh
+echo "hope this works"
+sleep 1
 
 rm /etc/pacman.conf
 cp ~/i3installconfig/pacman.conf /etc/pacman.conf
@@ -11,17 +14,16 @@ cd
 pacman -Syu
 mkdir ~/Tools
 mkdir ~/Tools/bin
-pacman -S i3-gaps i3blocks i3lock i3status polybar rofi firefox ntfs-3g pulseaudio lxappearance arandr kitty zsh newsboat neovim pulseaudio-alsa alsa-utils picom cmus redshiftbase-devel curl wget xdotool tmux
+pacman -S i3-gaps i3blocks i3lock i3status polybar rofi firefox ntfs-3g pulseaudio lxappearance arandr git kitty zsh newsboat neovim pulseaudio-alsa alsa-utils picom cmus redshiftbase-devel curl wget xdotool tmux
 
-wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-sh install.sh
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting /usr/share/zsh/plugins/fast-syntax-highlighting/
 cd
 git clone https://github.com/Waishnav/Watcher
 cd ./Watcher/
 ./install
 cd
 yay -S cava backlight_control plata-theme ttf-all-the-icons
-
+cp ~/i3installconfig/zshrc ~/.zhsrc
 cp ~/i3installconfig/scripts/* ~/Tools/bin
 cp ~/i3installconfig/configs ~/.config
 reboot
