b14685b2eb56ce1b9c50a34ee1a1d8e16effc879
SometimesSquishy <91513394+SometimesSquishy@users.noreply.github.com>
2022-04-29 18:36:37 -0400

    Update yt-dlp.sh

--- scripts/yt-dlp.sh
+++ scripts/yt-dlp.sh
@@ -12,4 +12,4 @@ read -r URL
 # Setting variables such as the URL and where to organize music to
 
 yt-dlp  -f 'ba' -x -P ~/Music/$ARTIST/$ALBUM --embed-thumbnail --cookies-from-browser firefox --add-metadata --audio-format m4a "%(title)s.%(ext)s" --ppa "EmbedThumbnail+ffmpeg_o:-c:v mjpeg -vf crop=\"'if(gt(ih,iw),iw,ih)':'if(gt(iw,ih),ih,iw)'\"" $URL
-# -ppa string makes a 1x1 thumbnail
+# -ppa string should make a 1x1 thumbnail but if i use m4a it dont work, but if i use mp3 i get much worse quality
