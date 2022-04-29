bed386d1b65e717ae5132da63246d9564adbdc6a
SometimesSquishy <91513394+SometimesSquishy@users.noreply.github.com>
2022-04-29 16:29:44 -0400

    Update yt-dlp.sh

--- scripts/yt-dlp.sh
+++ scripts/yt-dlp.sh
@@ -9,6 +9,6 @@ echo "Album?"
 read -r ALBUM
 echo "URL?"
 read -r URL
-
+# Setting variables such as the URL and where to organize music to
 
 yt-dlp  -f 'ba' -x -P ~/Music/$ARTIST/$ALBUM --embed-thumbnail --cookies-from-browser firefox --add-metadata --audio-format m4a "%(title)s.%(ext)s" --ppa "EmbedThumbnail+ffmpeg_o:-c:v mjpeg -vf crop=\"'if(gt(ih,iw),iw,ih)':'if(gt(iw,ih),ih,iw)'\"" $URL
