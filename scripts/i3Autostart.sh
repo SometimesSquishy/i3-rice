0fd9964b0ecba7d03704e3c8637bc90485f26353
SometimesSquishy <91513394+SometimesSquishy@users.noreply.github.com>
2022-05-12 09:58:35 -0400

    Update i3Autostart.sh

--- scripts/i3Autostart.sh
+++ scripts/i3Autostart.sh
@@ -2,30 +2,31 @@
 #Workspace startup stuff 
 #remember to chmod +x scripts and put them in ~/Tools/bin/
 
-# Setup Workspace 1
-i3-msg "workspace 1; exec st -f 'Roboto mono:size=9:antialiasing=true' newsboat"
+WORKSPACE=1
+TERMINAL="st -f 'Roboto mono:size=9:antialiasing=true'"
+
+# Setup Workspace
+i3-msg "workspace $WORKSPACE; exec $TERMINAL  newsboat"
 sleep 0.2;
-i3-msg "workspace 1; floating disable"
-i3-msg "workspace 1; split h"
-i3-msg "workspace 1; exec st -f 'Roboto mono:size=9:antialiasing=true' htop"
+i3-msg "workspace $WORKSPACE; floating disable"
+i3-msg "workspace $WORKSPACE; split h"
+i3-msg "workspace $WORKSPACE; exec $TERMINAL htop"
 sleep 0.2;
 i3-msg "resize shrink left 200 px"
 sleep 0.2;
-i3-msg "workspace 1; split v"
-i3-msg "workspace 1; exec st -f 'Roboto mono:size=9:antialiasing=true' cbonsai -l --life=40 --multiplier=3 -i"
-sleep 0.2;
+i3-msg "workspace $WORKSPACE; split v"
+i3-msg "workspace $WORKSPACE; exec $TERMINAL cbonsai -l --life=40 --multiplier=3 -i"
+$WORKSPACEleep 0.2;
 
 # Music Setup, Uncomment if dual monitors
-# exec bash ~/Tools/bin/muzix.sh
-
-# Back to Workspace 1
-i3-msg "workspace 1"
+# exec bash ~/Tools/bi$WORKSPACEmuzix.sh
+$WORKSPACE# Back to Workspace 1
+i3-msg "workspace $WORKSPACE"
 
 #----------------------------------------#
 # General app launch ....                #
 #----------------------------------------#
-
-# Launch Bar
+$WORKSPACE# Launch Bar
 exec polybar mystatus -q &
 #sleep 0.2; 
 
