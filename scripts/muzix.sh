a4b9386042e38b3874305e6d2156d2b3dbfc6021
SometimesSquishy <91513394+SometimesSquishy@users.noreply.github.com>
2022-09-09 21:46:42 -0400

    Update muzix.sh

--- scripts/muzix.sh
+++ scripts/muzix.sh
@@ -5,16 +5,16 @@
 # workspace to exec on
 WORKSPACE=3
 
-i3-msg "workspace $WORKSPACE; exec kitty alsamixer"
+i3-msg "workspace $WORKSPACE; exec st alsamixer"
 sleep 0.2;
 #i3-msg "resize
 i3-msg "workspace $WORKSPACE; flating disable"
 i3-msg "workspace $WORKSPACE; split h"
-i3-msg "workspace $WORKSPACE; exec kitty cmus"
+i3-msg "workspace $WORKSPACE; exec st cmus"
 sleep 0.2;
 i3-msg "workspace $WORKSPACE; floating disable"
 i3-msg "workspace $WORKSPACE; split v"
-i3-msg "workspace $WORKSPACE; exec kitty cava"
+i3-msg "workspace $WORKSPACE; exec st cava"
 sleep 0.2;
 i3-msg "workspace $WORKSPACE; floating disable"
 for i in {1..6}
