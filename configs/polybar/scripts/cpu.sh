03b3d8a00499078c5134382e243b4742956fff92
sometimessquishy <sebastian.arruda06@gmail.com>
2023-01-19 14:57:17 -0500

    This actually works unlike nproc, i musta been smoking crack

--- configs/polybar/scripts/cpu.sh
+++ configs/polybar/scripts/cpu.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 
-NPROCSTAT=$(grep 'cpu ' /proc/stat; nproc)
 # Need to test to see if it is always the 12th variable in /proc/stat cpu cuz if it isnt that could mess it up on some machines
 # also would be nice to make it keep the core number in a persistent variable so i dont need to run nproc every time this runs
 # as that could cause an unneeded load on the system
@@ -13,8 +12,7 @@ case "$1" in
         ;;
     *)
 
-        echo " $(echo $NPROCSTAT | awk '{cpu_usage=($2+$4)*100/($2+$4+$5)/$12}
-        END {printf "%0.2f%", cpu_usage}')
+        echo " $(top -n 1 -b | awk '/^%/{print $2"%"}')
          $(sensors | grep temp1 | head -1 | awk '{print $2}')"
         ;;
 esac
